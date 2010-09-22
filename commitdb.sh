e1308b90a31ac6b1df7e6f67203ced24548c7ac3
Claus <claus@codewerks.de>
2010-09-22 11:48:48 -0700

    Adding latest db script

--- commitdb.sh
+++ commitdb.sh
@@ -1,9 +1,9 @@
-"C:\Program Files (x86)\Microsoft SQL Server\90\Tools\Publishing\1.4\sqlpubwiz.exe" script -C "Data Source=(local);Initial Catalog=SongSearch2;Integrated Security=True;" "D:\DevProjects\SongSearch\SqlScripts\SongSearch2.sql" -targetserver 2008 -f
+"C:\Program Files (x86)\Microsoft SQL Server\90\Tools\Publishing\1.4\sqlpubwiz.exe" script -C "Data Source=(local);Initial Catalog=SongSearch2;Integrated Security=True;" "C:\DevProjects\SongSearch\SqlScripts\SongSearch2.sql" -targetserver 2008 -f
 cd SqlScripts
 tar -pvczf SongSearch2.tar.gz SongSearch2.sql
 rm SongSearch2.sql
 cd ..
 git add .
 git commit -am"Adding latest db script"
-git push origin dev
-git push github dev
+git push origin
+git push github
