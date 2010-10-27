b3ae6fbad1424ce0b9f7d3ae0fc252c9d71aaee0
Claus <claus@codewerks.de>
2010-10-26 21:25:46 -0700

    Changed logo support to png and added drop shadow

--- backup_production_db.sh
+++ backup_production_db.sh
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Microsoft SQL Server\90\Tools\Publishing\1.4\sqlpubwiz.exe" script -C "Data Source=www.worldsongnet.com, 1533;Initial Catalog=SongSearch2;user=deploy;password=W@rehous3;" "D:\DevProjects\SongSearch\SqlScripts\SongSearch2_ProductionBackup.sql" -targetserver 2008 -f
+"C:\Program Files (x86)\Microsoft SQL Server\90\Tools\Publishing\1.4\sqlpubwiz.exe" script -C "Data Source=www.worldsongnet.com, 1533;Initial Catalog=SongSearch2;user=deploy;password=W@rehous3;" "C:\DevProjects\SongSearch\SqlScripts\SongSearch2_ProductionBackup.sql" -targetserver 2008 -f
 cd SqlScripts
 tar -pvczf SongSearch2_ProductionBackup.tar.gz SongSearch2_ProductionBackup.sql
 rm SongSearch2_ProductionBackup.sql
